4b5613c4cb52b42d51ceb0cf3a7e2b7c2ef39c58
Kang-min Liu <gugod@gugod.org>
2011-07-14 10:30:18 +0800

    done an file upload program with Anyblob::Client

--- lib/Anyblob/Client.pm
+++ lib/Anyblob/Client.pm
@@ -2,11 +2,27 @@ package Anyblob::Client;
 use 5.012;
 use Moose;
 
+has blob_size => (
+    is => "ro",
+    isa => "Int",
+    default => 512 * 1024
+);
+
 has server => (
     is => "ro",
     isa => "Str",
     required => 1
 );
+
+has ua => (
+    is => "ro",
+    isa => "LWP::UserAgent",
+    lazy_build => 1,
+);
+
+sub _build_ua {
+    return LWP::UserAgent->new;
+}
 
 use Anyblob::Helpers;
 use LWP::UserAgent;
@@ -14,19 +30,24 @@ use HTTP::Request;
 
 sub check {
     my ($self, $ref) = @_;
+    my $response = $self->ua->request(HTTP::Request->new(HEAD => $self->server . "/blobs/$ref"));
+    return $response->is_success;
+
 }
 
 sub store {
     my ($self, $blob) = @_;
     my $ref = blobref($blob);
     return 1 if ($self->check($ref));
-    my $ua = LWP::UserAgent->new;
-    my $response = $ua->request(HTTP::Request->new(PUT => $self->server . "/blobs/$ref", [], $blob));
+    my $response = $self->ua->request(HTTP::Request->new(PUT => $self->server . "/blobs/$ref", [], $blob));
     return ($response->is_success);
 }
 
 sub retrieve {
     my ($self, $ref) = @_;
+    my $response = $self->ua->request(HTTP::Request->new(GET => $self->server . "/blobs/$ref"));
+    return undef unless $response->is_success;
+    return $response->content;
 }
 
 sub list {
@@ -35,7 +56,28 @@ sub list {
 
 sub upload {
     my ($self, $file) = @_;
+
+    my @refs;
+
+    my $blob;
+    open my $fh, "<:bytes", $file;
+
+    while (read($fh, $blob, $self->blob_size)) {
+        my $ref = blobref($blob);
+
+        unless($self->store($blob)) {
+            say STDERR "ERROR storing $ref";
+            return undef;
+        }
+
+        push @refs, $ref;
+    }
+
+    close($fh);
+
+    return wantarray ? @refs : \@refs;
 }
+
 
 sub download {
     my ($self, $filename, $refs) = @_;
